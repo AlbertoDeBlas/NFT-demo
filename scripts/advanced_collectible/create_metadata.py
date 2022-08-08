b7405975c832dfe7c3bf739ed9ff58b78e8c161e
AlbertoDeBlas <alberto.de.blas@gmail.com>
2022-08-08 13:01:34 +0200

    Upgrade VRF to version 2

--- scripts/advanced_collectible/create_metadata.py
+++ scripts/advanced_collectible/create_metadata.py
@@ -1,7 +1,10 @@
 from brownie import AdvancedCollectible
+from scripts.helpful_scripts import get_breed
 
 
 def main():
     advanced_collectible = AdvancedCollectible[-1]
     number_of_advanced_collectibles = advanced_collectible.tokenCounter()
     print(f"You have created {number_of_advanced_collectibles} collectibles!")
+    for token_id in range(number_of_advanced_collectibles):
+        breed = get_breed(advanced_collectible.tokenIdToBreed(token_id))
